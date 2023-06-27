4383b1cf3341fc586c360406cefde24f6801134c
= <=>
2023-06-27 08:14:38 -0300

    Actualización de Prueba.py, practica de funciones

--- Prueba.py
+++ Prueba.py
@@ -1,28 +1,50 @@
+#Funciones
 
-"""
-palabras=['pato','carro','pelota','anillo']
+temperatura=4
+def miFuncion(nombre):
+     print(f"Hola como estás, {nombre}")
+     for i in range(1, 11):
+          print(i)
+          print("Temperatura: ", temperatura)
 
-for i in palabras:
-     print(i)
-
- 
-print(f'La lista termina en la palabra {i}')
-
-"""
-num=1
-
-while num <= 10:
-     num += 1 
-     if num % 2 == 0:
-          print (f"El número {num} es par...")
-          continue
-
-print('Fin del bucle')
-
-lista=['1','2','3','u','f']
-
-for i in lista:
-     print('Elemento ', i, 'de la lista...')
+def suma(a=1,b=0,c=0):
+     print(f"El resultado de la suma es: {a+b+c}")
+     print("Primer parametro: ",a)
+     print("Primer parametro: ",b)
+     print("Primer parametro: ",c)
      
-long=len(lista)
-print('La lista tiene ', long, ' items....')
+def sumaNumeros(*args):
+     suma=0
+     for i in args:
+          suma += i
+     print("Total", suma)
+
+
+def miDiccionario(**kwargs):
+     for key, value in kwargs.items():
+          print(key,"=",value)
+
+
+def operaciones (a,b):
+     return a+b, a-b, a*b, a/b
+
+miFuncion("Julian")
+
+suma(a=10,b=10,c=1)
+
+#envía a la función n cantidad de parametros
+sumaNumeros(1,2,3,4,5,9,10,11,20,50)
+
+miDiccionario(vivienda="piso",auto='rojo')
+
+
+# funcion con multiples resultados
+suma, resta, multi, divi = operaciones(20,10)
+print('Función con multiples resultados')
+print(suma)
+print(resta)
+print(multi)
+print(divi)
+
+
+
